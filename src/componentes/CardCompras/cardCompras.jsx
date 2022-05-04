5e856dab4af13c42dfef85ffa20b00d90fa3581f
Walquiria Godoy <walquiriagodoy@gmail.com>
2022-05-04 10:24:24 -0300

    feat(card-compras): Add cardCompras css file

--- src/componentes/CardCompras/cardCompras.jsx
+++ src/componentes/CardCompras/cardCompras.jsx
@@ -1,30 +1,31 @@
-import React, { Component } from "react";
-import "./cardCompras.css";
+import React, { Component } from 'react';
+import './cardCompras.css';
 import logolab2 from '../../assets/img/logolab2.png';
 
-class CardCompras extends Component{
+class CardCompras extends Component {
+  _turmaSelecionada(evento) {
+    evento.stopPropagation();
+    const turmaSelecionadaId = evento.currentTarget.id;
+    this.props.turmaSelecionada(turmaSelecionadaId);
+  }
 
-    _turmaSelecionada(evento){
-        evento.stopPropagation();
-        const turmaSelecionadaId = evento.currentTarget.id;
-        this.props.turmaSelecionada(turmaSelecionadaId)
-    }
-
-    render(){
-        return(
-            <div className="lista-cards">
-                TESTE
-                {/* TODO: mudar id  da section para usar data-atribute 
-                <section className="card" id={this.props.id} onClick={this._turmaSelecionada.bind(this)}>
-                    <h1 className="card-nome-curso">{this.props.curso}</h1>
-                    <img className="card-imagem" src={logolab2}/>
-                    <h2 className="card-turma">{this.props.turma}</h2>
-                    <h3 className="card-dia-hora">{this.props.dia}</h3>
-                    <h3 className="card-preco">{this.props.preco}</h3>
-                </section>*/}
-            </div>
-            );
-        }
-    }
+  render() {
+    return (
+      <div className='lista-card-compras'>
+        <section
+          className='card-compras'
+          id={this.props.id}
+          onClick={this._turmaSelecionada.bind(this)}
+        >
+          <h1 className='card-nome-curso'>{this.props.curso}</h1>
+          <img className='card-compras-imagem' src={logolab2} />
+          <h2 className='card-turma'>{this.props.turma}</h2>
+          <h3 className='card-dia-hora'>{this.props.dia}</h3>
+          <h3 className='card-preco'>{this.props.preco}</h3>
+        </section>
+      </div>
+    );
+  }
+}
 
 export default CardCompras;
--- src/componentes/CarrinhoDeCompras/carrinhoDeCompras.jsx
+++ src/componentes/CarrinhoDeCompras/carrinhoDeCompras.jsx
@@ -1,43 +1,40 @@
-import React, { Component } from "react";
-import CardCompras from "../CardCompras/cardCompras.jsx";
+import React, { Component } from 'react';
+import CardCompras from '../CardCompras/cardCompras.jsx';
 import './carrinhoDeCompras.css';
 
-class CarrinhoDeCompras extends Component{
+class CarrinhoDeCompras extends Component {
+  constructor() {
+    super();
+    this.state = { turmasNoCarrinho: [] };
+  }
 
-    constructor(){
-        super();
-        this.state = {turmasNoCarrinho: []};
-    }
+  componentDidMount() {
+    this.props.turmas.inscrever1(this.turmasParaCompra.bind(this));
+  }
 
-    componentDidMount(){
-        this.props.turmas.inscrever1(this.turmasParaCompra.bind(this));
-    }
-    
-    componentWillUnmount(){
-        this.props.turmas.desinscrever1(this.turmasParaCompra.bind(this));
-    }
-    turmasParaCompra(turmasNoCarrinho){
-        debugger
-        this.setState({turmasNoCarrinho});
-    }
+  componentWillUnmount() {
+    this.props.turmas.desinscrever1(this.turmasParaCompra.bind(this));
+  }
+  turmasParaCompra(turmasNoCarrinho) {
+    this.setState({ turmasNoCarrinho });
+  }
 
-    render(){
-        return(
-            <>
-                <h1 className="titulo-principal">Meu Carrinho de Compras</h1>
-                {console.log(this.state.turmasNoCarrinho)}
-                <ul>
-                {this.state.turmasNoCarrinho.map((turma, index) => {
-                    return(
-                        <li className="caixa-cards" key={index}>
-                            <CardCompras
-                            turmas={turma}/>
-                        </li>
-                    );
-                })}
-            </ul>
-            </>
-        )
-    }
+  render() {
+    return (
+      <>
+        <h1 className='titulo-principal'>Meu Carrinho de Compras</h1>
+        {console.log(this.state.turmasNoCarrinho)}
+        <ul className='lista-compras'>
+          {this.state.turmasNoCarrinho.map((turma, index) => {
+            return (
+              <li className='caixa-card-compras' key={index}>
+                <CardCompras turmas={turma} />
+              </li>
+            );
+          })}
+        </ul>
+      </>
+    );
+  }
 }
 export default CarrinhoDeCompras;
